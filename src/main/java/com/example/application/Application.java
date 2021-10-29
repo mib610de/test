22e959300381798e544c97c17abfd90b79dbc0da
start.vaadin.com <no-reply@vaadin.com>
2021-10-29 10:38:51 +0000

    Generated project

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,34 @@
+package com.example.application;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "menuapp")
+@PWA(name = "MenuApp", shortName = "MenuApp", offlineResources = {"images/logo.png"})
+@Push
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+@NpmPackage(value = "leaflet", version = "^1.7.1")
+@NpmPackage(value = "@types/leaflet", version = "^1.5.23")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+    }
+
+}
---
+++ src/main/java/com/example/application/views/map/MapView.java
@@ -0,0 +1,24 @@
+package com.example.application.views.map;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.example.application.components.leafletmap.LeafletMap;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import com.example.application.views.MainLayout;
+import javax.annotation.security.PermitAll;
+
+@PageTitle("Map")
+@Route(value = "map", layout = MainLayout.class)
+@PermitAll
+public class MapView extends VerticalLayout {
+
+    private LeafletMap map = new LeafletMap();
+
+    public MapView() {
+        setSizeFull();
+        setPadding(false);
+        map.setSizeFull();
+        map.setView(55.0, 10.0, 4);
+        add(map);
+    }
+}
